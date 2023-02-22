97d91ba77ff759f93e4c4d1eccfc726d606b873b
sepehr9877 <sepehr.seifpour@yahoo.com>
2023-02-22 13:07:27 -0500

    Adding login and regitration and updating user

--- Account/API/serializer.py
+++ Account/API/serializer.py
@@ -39,3 +39,48 @@ class RegistrationSeralizer(Serializer):
             phonenumber=phone,
         )
         return email,password
+
+
+class UpdateSerializer(Serializer):
+    first_name = CharField(max_length=50, required=True)
+    last_name = CharField(max_length=50, required=True)
+    phonenumber = CharField(max_length=50, required=True)
+    email = EmailField(max_length=50, required=True)
+    birth = DateField(required=True)
+    username=CharField(max_length=50,required=True)
+    address=CharField(max_length=50,required=True)
+    rigrationumber=CharField(required=False)
+
+    def get_initial(self):
+        select_user=self.context['user']
+        initial={
+            "first_name":select_user.first_name,
+            "username":select_user.username,
+            "last_name":select_user.last_name,
+            "email":select_user.email,
+            "phonenumber":select_user.phonenumber,
+            "birth":select_user.birth,
+            "address":select_user.address,
+        }
+        return initial
+    def update(self,validated_data,user):
+        firstname=validated_data["first_name"]
+        lastname=validated_data["last_name"]
+        username=validated_data["username"]
+        phonumber=validated_data["phonenumber"]
+        birth=validated_data["birth"]
+        address=validated_data["address"]
+        email=validated_data["email"]
+        id=user.id
+        update_user=CustomUser.objects.filter(id=id).update(
+            first_name=firstname,
+            last_name=lastname,
+            address=address,
+            phonenumber=phonumber,
+            birth=birth,
+            email=email,
+            username=username
+        )
+        selected_updated_user=CustomUser.objects.filter(id=id).first()
+        return selected_updated_user
+
