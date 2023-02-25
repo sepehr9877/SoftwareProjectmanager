a8657cd2f1aa927873f881b4c0c5553c07d9538d
sepehr9877 <sepehr.seifpour@yahoo.com>
2023-02-24 22:51:52 -0500

    new changes

--- Counselor/API/serializer.py
+++ Counselor/API/serializer.py
@@ -11,29 +11,45 @@ class CounselorPatientAppointmentSerializer(Serializer):
     AssignedToDoctor=BooleanField(read_only=True,default=False)
     def to_representation(self, instance):
         data=super().to_representation(instance)
-        data['AssignedToDoctor']=data.get('AssignedToDoctor')
+        request_data=self.context['data']
         counselor_email = self.context['authuser'].first().email
         selected_counselor=CustomUser.objects.filter(email__exact=counselor_email).first()
         CounselorData={}
         CounselorData['first_name']=selected_counselor.first_name
         CounselorData['last_name']=selected_counselor.last_name
         CounselorData['email']=selected_counselor.email
-        patient_email=data.get('Patients')
+        patient_email=request_data.get('Patients')
         if  patient_email :
-            selected_patients=CounselorAppointment.objects.filter(Counselor_id=selected_counselor.id).first()
+            selected_patients=CounselorAppointment.objects.filter(Counselor_id=selected_counselor.id,Patients__email=patient_email).all()
         else:
-            selected_patients=CustomUser.objects.filter(Counselor_id=selected_counselor.id).all()
+            selected_patients=CounselorAppointment.objects.filter(Counselor_id=selected_counselor.id).all()
         PatientList=[]
-
+        selected_patients_len=selected_patients.count()
         PatientsData={}
-        if selected_patients:
-            for patient in selected_patients:
-                PatientsData['first_name'] = patient.first_name
-                PatientsData['last_name'] = patient.last_name
-                PatientsData['email'] = patient.email
+        Result=[]
+        if selected_patients_len>1:
+
+            for patients in selected_patients:
+                patient=patients.first()
+                PatientsData={}
+                PatientsData['first_name'] = patient.Patients.first_name
+                PatientsData['last_name'] = patient.Patients.last_name
+                PatientsData['email'] = patient.Patients.email
+                data['Appointment']=patient.Appointment
                 PatientList.append(PatientsData)
+        elif selected_patients:
+            selected_patients=selected_patients.first()
+            PatientsData['first_name'] = selected_patients.Patients.first_name
+            PatientsData['last_name'] = selected_patients.Patients.last_name
+            PatientsData['email'] = selected_patients.Patients.email
+            data['Appointment'] = selected_patients.Appointment
+            PatientList.append(PatientsData)
+        else:
+            data['Appointment']=None
+
         data['Counselor']=CounselorData
         data['Patients']=PatientList
+        Result.append(data)
         return data
     def validate(self,data):
         CounselorAuth=self.context['authuser'].first()
@@ -56,8 +72,9 @@ class CounselorPatientAppointmentSerializer(Serializer):
         selected_patient=self.context['SelectedPatients'].first()
         selected_counslor=self.context['SelectedCounselor'].first()
         created_item_id=None
-
-        if (AssignedToDoctor is None) and ((selected_item.first().Counselor.id==selected_counslor.id )or (selected_item.first().Counselor.id is None)):
+        print(selected_item)
+        print(selected_counslor)
+        if (AssignedToDoctor is None) and ((selected_item.first().Counselor==selected_counslor)or (selected_item.first().Counselor is None)):
             created_item = CounselorAppointment.objects.update(
                 Counselor_id=selected_counslor.id,
                 Patients_id=selected_patient.id,
