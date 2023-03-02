f37355a369f6373301dcfcdb3a0746f4e9e2d63f
sepehr9877 <sepehr.seifpour@yahoo.com>
2023-03-02 01:13:54 -0500

    edit a url

--- Account/API/urls.py
+++ Account/API/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegisterApi,UpdateUserApi,UpdatePasswordEmailApi,LoginApi,GetAllDetail
 urlpatterns=[
     path('Register',RegisterApi.as_view()),
-    path('UpdateUserApi/',UpdateUserApi.as_view()),
+    path('UpdateUserApi',UpdateUserApi.as_view()),
     path('UpdatePasswordEmail',UpdatePasswordEmailApi.as_view()),
     path('login/',LoginApi.as_view()),
     path('GetAllDetail',GetAllDetail.as_view())
